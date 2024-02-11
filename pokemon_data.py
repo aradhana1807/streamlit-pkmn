4e5aeb3f56803bb9f8a17bdf15773f4234e220e6
aradhana1807 <aradhana.zero@gmail.com>
2024-02-11 13:26:36 +0530

    pushing changes

--- pokemon_data.py
+++ pokemon_data.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 
 df = pd.read_csv("pokemon.csv")
@@ -10,25 +11,18 @@ st.write("# Pokemon Dataset")
 st.dataframe(df, use_container_width=True)
 
 for generation in range(1, 7):
-    # Filtering Pokémon for the current generation
+    # Filter Pokémon for the current generation
     gen_df = df[df['Generation'] == generation]
 
+    # Count the number of Pokémon with one type and two types
     one_type_count = gen_df['Type 2'].isna().sum()
     two_types_count = (~gen_df['Type 2'].isna()).sum()
 
-    # Creating a pie chart
-    labels = ['Type I', 'Type I/II']
-    explode = (0.1, 0)
-    colors = ['#D04848', '#6895D2']
-    sizes = [one_type_count, two_types_count]
-    fig, ax = plt.subplots(figsize=(5, 4))
-    ax.pie(sizes, explode=explode, labels=labels, colors=colors, shadow=True, autopct='%1.1f%%', startangle=90)
-    ax.axis('equal')  
-    ax.set_title(f'Generation {generation} Pokémon: One Type vs Two Types')
+    # Create a pie chart with Plotly
+    labels = ['One Type', 'Two Types']
+    values = [one_type_count, two_types_count]
+    fig = px.pie(names=labels, values=values, title=f'Generation {generation} Pokémon: One Type vs Two Types')
 
-    # Reducing padding around the pie chart
-    plt.tight_layout(pad=1.0)
-
-    # Displaying the chart in Streamlit
+    # Display the chart in Streamlit
     st.write(f"## Generation {generation} Pokémon:")
-    st.pyplot(fig)
+    st.plotly_chart(fig)
