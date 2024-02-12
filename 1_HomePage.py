db776cb4dde64c489355599af7c4bee602361683
Aradhana Nayak <aradhana.zero@gmail.com>
2024-02-12 15:05:54 +0000

    added nav links

--- 1_HomePage.py
+++ 1_HomePage.py
@@ -13,9 +13,35 @@ centered_markdown = """
     <br>Whether you're a seasoned trainer or just starting your journey, this is the place to be!
     </p>
 </div>
+
+
 """
 
 with open( "style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 # Display centered markdown
 st.markdown(centered_markdown, unsafe_allow_html=True)
+
+
+nav_markdown = """
+<h3>
+You can explore using below links!
+</h3>
+
+"""
+
+st.markdown(nav_markdown, unsafe_allow_html=True)
+# Function to generate buttons
+def page_link_button(page_name, button_label):
+    return f'<a href="/{page_name}" style="text-decoration: none;"><button class="transparent-button">{button_label}</button></a>'
+
+# Define your pages
+pages = {
+    '2_SearchIndex': 'Search Index',
+    '3_PokemonAnalysis': 'Pokemon Analysis',
+    # Add more pages as needed
+}
+
+# Display buttons for each page
+for page_name, button_label in pages.items():
+    st.markdown(page_link_button(page_name, button_label), unsafe_allow_html=True)
